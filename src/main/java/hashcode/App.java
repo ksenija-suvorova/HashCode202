3a5c1b1676520ff4c942f613e3750ade23f498b3
Ksenija Suvorova <ksenijainthenight@gmail.com>
2020-02-20 21:49:12 +0200

    Refacrtoring

--- src/main/java/hashcode/App.java
+++ src/main/java/hashcode/App.java
@@ -1,12 +1,17 @@
 package hashcode;
 
 import hashcode.domain.Book;
+import hashcode.domain.Library;
+import org.apache.commons.io.FileUtils;
+import sun.rmi.server.InactiveGroupException;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class App
 {
@@ -14,46 +19,52 @@ public class App
     {
 
         Integer rowsCount = 0;
-        List<Book> books = null;
+        Set<Book> books = null;
         Integer booksCount = null;
         Integer libraryCount;
         Integer days;
+        int character=3;
+        int lines = 0;
 
-        String fileName = "/a_example.txt";
-        //ClassLoader classLoader = new App().getClass().getClassLoader();
+        File file = new File(App.class.getClassLoader().getResource("/a_example.in").getFile());
 
-        File file = new File(new App().getClass().getResource(fileName).getFile());
+        String content = FileUtils.readFileToString(file);
 
-        BufferedReader reader;
+        String[] line = content.split("\n");
 
-        reader = new BufferedReader(new FileReader(file));
+        String[] initialLine = line[lines].split(" ");
+        lines++;
 
-        String line = reader.readLine();
+        booksCount = Integer.valueOf(initialLine[0]);
+        libraryCount = Integer.valueOf(initialLine[1]);
+        days = Integer.valueOf(initialLine[2]);
 
-        while (line != null){
-            if (rowsCount == 0) {
+        String[] booksScore = line[lines].split(" ");
+        lines++;
 
-                booksCount = Integer.valueOf(line.charAt(0));
-                libraryCount = Integer.valueOf(line.charAt(2));
-                days = Integer.valueOf(line.charAt(4));
-                rowsCount++;
-            }
-            else if(rowsCount == 1){
-                int character = 0;
-                for (int index=0; index<booksCount; index++){
-                    Book book = new Book(index+1, line.charAt(character));
-                    books.add(book);
-                }
-                rowsCount++;
-            }
-            else {
-
-            }
+        for (int index=1; index<=booksScore.length; index++){
+            Book book = new Book(index,Integer.valueOf(booksScore[index-1]));
+            books.add(book);
         }
 
+        for (int library = 0; library<libraryCount; library++){
+            Set<Integer> booksIds = null;
+            String[] libraryInformation = line[lines].split(" ");
+            lines++;
+            String[] libraryBooks = line[lines].split(" ");
+            lines++;
+
+            for (int i=0; i<libraryBooks.length; i++){
+                booksIds.add(Integer.valueOf(libraryBooks[i]));
+            }
+
+            List<String> a = new ArrayList<>(books);
 
 
-        reader.close();
+
+            Library librarys = new Library(library, Integer.valueOf(libraryInformation[1]), Integer.valueOf(libraryInformation[2]), booksIds, null)
+
+        }
 
     }
 }
